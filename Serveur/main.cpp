eebb2b34eae08686276589c6cec7fbc066bed264
benjisora <benji.saugues@gmail.com>
2016-03-15 17:38:18 +0100

    Commit Server with listening

--- Serveur/main.cpp
+++ Serveur/main.cpp
@@ -17,7 +17,6 @@ int main()
     std::vector<std::unique_ptr<sf::TcpSocket>> clients;
     std::vector<string> pseudos;
 
-
     int idClient = 0;
     int tailleGrille = 0, nbAlign = 0;
     string client2IP = "";
@@ -41,25 +40,25 @@ int main()
 
                     selector.add(*clients[clients.size()-1]); // Ajout du client au selecteur de telle sorte a ce qu'on puisse l'écouter
 
-                    cout << "NOUVEAU CLIENT !! nombre de clients: " << clients.size() <<endl;
+                    cout << "NOUVEAU CLIENT !! Nombre de clients: " << clients.size() <<endl;
 
                     sf::Packet packet;
                     if (clients[idClient]->receive(packet) != sf::Socket::Done){
                         cout << "echec de reception du pseudo" <<endl;
                     }
                     string pseudo;
-                    packet >> pseudo;
-                    packet.clear();
-                    pseudos.push_back(pseudo);
+                    packet >> code >> pseudo;
 
-                    cout << pseudos[0]<<endl;
-
-                    data << 101 + idClient;
-                    cout << 101 + idClient << endl;
-                    if(clients[idClient]->send(data) != sf::Socket::Done){
-                        cout << "echec d'envoi de l'ID du joueur" <<endl;
+                    if(code == 0){
+                        packet.clear();
+                        pseudos.push_back(pseudo);
+                        cout << pseudos[idClient]<<endl;
                     }
-
+                    code = 0;
+                    data << code;
+                    if(clients[idClient]->send(data) != sf::Socket::Done){
+                        cout << "echec d'envoi du code" <<endl;
+                    }
                     data.clear();
 
                     idClient++;
@@ -71,18 +70,29 @@ int main()
             else{ // La socket du listener n'est pas prete, on teste tous les autres sockets (clients)
 
                 for (int i=0; i < clients.size();i++){
-
+                        cout << "boucle for"<<endl;
                     if (selector.isReady(*clients[i].get())){
                         // Le client a fait quelque chose dans la socket
-
+                        cout << "selectoready"<<endl;
                         sf::Packet packet;
                         if (clients[i]->receive(packet) != sf::Socket::Done){
                             cout <<"Reception d'un paquet du client"<<endl;
                         }
-
-                        packet >> tailleGrille >> client2IP >> nbAlign;
+                        packet >> code >> tailleGrille >> client2IP >> nbAlign;
                         packet.clear();
-                        cout << tailleGrille <<  " " << client2IP << " " << nbAlign << endl;
+
+                        if(code == 11){
+                            //delete clients[i].get();
+
+                            clients.erase(clients.begin()+i);
+                            pseudos.erase(pseudos.begin()+i);
+                            cout << "Client " << i << " est parti..." <<endl;
+                            tailleGrille = 0;
+                            client2IP = "";
+                            nbAlign = 0;
+                            //system("pause");
+                        }
+
                     }
                 }
             }
