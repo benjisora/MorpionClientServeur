83f79d6904d7e68d073496d7d494130e293dc771
Neragoth <mobitop71@gmail.com>
2016-03-15 11:17:11 +0100

    base du client

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,10 +1,35 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <iostream>
+#include <random>
+#include <thread>
+#include <sstream>
+
+using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::TcpSocket socket;
+    string ip = "127.0.0.1";
+    string port = "";
+    int portval = 4390;
+
+    /****************************
+    CREATE
+    *****************************/
+
+    cout << "Client start" << endl;
+    //Demande des infos de connexion
+    cout << "Entrez IP : ";
+    getline(cin, ip);
+    cout << "Entrez port : ";
+    getline(cin, port);
+
+    stringstream buffer(port);
+    buffer >> portval;
+
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML works!");
+
 
     while (window.isOpen())
     {
@@ -14,9 +39,16 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        /****************************
+        STEP
+        *****************************/
 
         window.clear();
-        window.draw(shape);
+
+        /****************************
+        DRAW
+        *****************************/
+
         window.display();
     }
 
