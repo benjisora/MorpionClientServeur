4464860a7926a937496a3a96d6f40b2c318bd2a9
Neragoth <mobitop71@gmail.com>
2016-03-17 09:20:30 +0100

    Modification inutile

--- Client/main.cpp
+++ Client/main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main()
 {
     sf::TcpSocket socket;
-    string ip = "172.31.247.41";
+    string ip = "127.0.0.1";
     string portString = "";
     string pseudo ="";
     int port = 80;
--- Serveur/main.cpp
+++ Serveur/main.cpp
@@ -26,16 +26,20 @@ int main()
     sf::SocketSelector selector;
     selector.add(listener); //Création du select, et ajout du listener au select
 
-    while (true){
+    while (true)
+    {
         // On attend un message d'une des sockets
-        if (selector.wait()){
+        if (selector.wait())
+        {
             // Test du listener
-            if (selector.isReady(listener)){ // Il y a une connexion en attente
+            if (selector.isReady(listener))  // Il y a une connexion en attente
+            {
 
                 unique_ptr<sf::TcpSocket> client = make_unique<sf::TcpSocket>();
 
-                if (listener.accept(*client.get()) == sf::Socket::Done){
-
+                if (listener.accept(*client.get()) == sf::Socket::Done)
+                {
+                    client->setBlocking(false);
                     clients.push_back(std::move(client));
 
                     selector.add(*clients[clients.size()-1]); // Ajout du client au selecteur de telle sorte a ce qu'on puisse l'écouter
@@ -43,56 +47,65 @@ int main()
                     cout << "NOUVEAU CLIENT !! Nombre de clients: " << clients.size() <<endl;
 
                     sf::Packet packet;
-                    if (clients[idClient]->receive(packet) != sf::Socket::Done){
+                    if (clients[idClient]->receive(packet) != sf::Socket::Done)
+                    {
                         cout << "echec de reception du pseudo" <<endl;
                     }
                     string pseudo;
                     packet >> code >> pseudo;
 
-                    if(code == 0){
+                    if(code == 0)
+                    {
                         packet.clear();
                         pseudos.push_back(pseudo);
                         cout << pseudos[idClient]<<endl;
                     }
                     code = 0;
                     data << code;
-                    if(clients[idClient]->send(data) != sf::Socket::Done){
+                    if(clients[idClient]->send(data) != sf::Socket::Done)
+                    {
                         cout << "echec d'envoi du code" <<endl;
                     }
                     data.clear();
 
                     idClient++;
                 }
-                else{
+                else
+                {
                     cout<<"connexion non acceptee"<<endl;
                 }
             }
-            else{ // La socket du listener n'est pas prete, on teste tous les autres sockets (clients)
+            else  // La socket du listener n'est pas prete, on teste tous les autres sockets (clients)
+            {
 
-                for (int i=0; i < clients.size();i++){
-                        cout << "boucle for"<<endl;
-                    if (selector.isReady(*clients[i].get())){
+                for (int i=0; i < clients.size(); i++)
+                {
+                    //cout << "boucle for"<<endl;
+                    if (selector.isReady(*clients[i].get()))
+                    {
                         // Le client a fait quelque chose dans la socket
-                        cout << "selectoready"<<endl;
+                        //cout << "selectoready"<<endl;
+
                         sf::Packet packet;
-                        if (clients[i]->receive(packet) != sf::Socket::Done){
-                            cout <<"Reception d'un paquet du client"<<endl;
+                        if (clients[i]->receive(packet))
+                        {
+                            cout << "receive" << endl;
+                            packet >> code >> tailleGrille >> client2IP >> nbAlign;
+                            packet.clear();
+
+                            if(code == 11)
+                            {
+                                //delete clients[i].get();
+
+                                clients.erase(clients.begin()+i);
+                                pseudos.erase(pseudos.begin()+i);
+                                cout << "Client " << i << " est parti..." <<endl;
+                                tailleGrille = 0;
+                                client2IP = "";
+                                nbAlign = 0;
+                                //system("pause");
+                            }
                         }
-                        packet >> code >> tailleGrille >> client2IP >> nbAlign;
-                        packet.clear();
-
-                        if(code == 11){
-                            //delete clients[i].get();
-
-                            clients.erase(clients.begin()+i);
-                            pseudos.erase(pseudos.begin()+i);
-                            cout << "Client " << i << " est parti..." <<endl;
-                            tailleGrille = 0;
-                            client2IP = "";
-                            nbAlign = 0;
-                            //system("pause");
-                        }
-
                     }
                 }
             }
